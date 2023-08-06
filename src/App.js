5bd97d2382522386b3a265ae45ff8c8482f3b536
rafiechautie <rafiqauti13@gmail.com>
2023-08-07 04:46:36 +0700

    initialization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const Hello = () => 'Hello'
+  return (
+    <>
+      <h1>{Hello()}</h1>
+    </>
+  );
+}
+
+export default App;
